dd067f26e8da377ec341689a8b975e7c009b0647
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-12-23 11:50:09 +0100

    Initial commit

---
+++ templates/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+func main()  {
+	tpl,err:=template.ParseFiles("templates/tpl.gohtml")
+	if err!=nil{
+		log.Fatalln(err)
+	}
+	nf,err:=os.Create("index.html")
+	if err!=nil{
+		log.Println("Error creating file",err)
+	}
+	defer nf.Close()
+	err=tpl.Execute(nf,nil)
+	if err!=nil{
+		log.Fatalln(err)
+	}
+}
